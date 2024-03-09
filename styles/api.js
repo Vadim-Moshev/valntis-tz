7e26f4e230f271972622356be0a061e134d5fac1
vadim-moshev <reg-vadim-moshev@yandex.ru>
2024-03-09 20:53:51 +0500

    add getHeaders function for auth purpose

--- styles/api.js
+++ styles/api.js
@@ -1,1 +1,14 @@
 const API_URL = "http://api.valantis.store:40000/";
+
+const getHeaders = () => {
+  const isoString = new Date().toISOString();
+  const dateComponents = isoString.split("T")[0];
+  const timestamp = dateComponents.replace(/-/g, "");
+
+  const authString = CryptoJS.MD5(`Valantis_${timestamp}`).toString();
+
+  return {
+    "X-Auth": authString,
+    "Content-Type": "application/json",
+  };
+};
